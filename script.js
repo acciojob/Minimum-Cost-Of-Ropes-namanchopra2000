c48b82799eedc498a5956670beccc46ea967af32
namanchopra2000 <117727607+namanchopra2000@users.noreply.github.com>
2023-02-25 03:51:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your code here
+function wow(event) {
+	event.preventDefault();
+	var a = document.querySelector("input").value
+    var r = a.split(",");
+
+var sum =0;
+    while(r.length>1){
+    r.sort( function (a , b ){ return a-b} );
+    var inssum =    Number(r[0])+Number(r[1]);
+    sum = sum + inssum
+    console.log(r);
+    r.splice(0 , 2 , inssum)
+    }
+
+	document.querySelector("#result").innerText = sum;
+}
